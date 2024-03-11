97a637aec28d4c6d8d5d39dd8ed8dfd6dcc3ff08
hoanghieutt <hhieu5764@gmail.com>
2024-03-11 11:11:54 +0700

    sửa thông tin hóa đơn lên thông báo khách hàng

--- DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/controller/admin/AddProductController.java
+++ DATN-SD-18-Website-Royal-Shirt/src/main/java/com/poly/datn/sd18/controller/admin/AddProductController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Arrays;
 import java.util.List;
 
 @Controller
@@ -30,6 +31,8 @@ public class AddProductController {
 
     @GetMapping("")
     public String getAll(Model model) {
+
+
         List<Brand> listBrand = brandService.getAllActive();
         model.addAttribute("listBrand", listBrand);
 
